45e64c6d3f9432d23359896b7315ac7805e3bd21
Marcelo Alves <marceloalves87@gmail.com>
2018-01-01 22:25:14 -0300

    Teste

--- EonSimulator/src/main/java/br/ufpe/eonsimulator/business/PSOSimulationController.java
+++ EonSimulator/src/main/java/br/ufpe/eonsimulator/business/PSOSimulationController.java
@@ -1,5 +1,9 @@
 package br.ufpe.eonsimulator.business;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,15 +15,16 @@ import br.ufpe.eonsimulator.domain.Simulation;
 import br.ufpe.eonsimulator.rsa.RSAWrapper;
 import br.ufpe.simulator.pso.IProblemSet;
 import br.ufpe.simulator.pso.Location;
+import br.ufpe.simulator.pso.PSOConstants;
 import br.ufpe.simulator.pso.PSOProcess;
 import br.ufpe.simulator.pso.ProblemRange;
+import br.ufpe.simulator.utils.ConvertUtils;
 
-public class PSOSimulationController extends AbstractSimulationController
-		implements IsSimulationController {
+public class PSOSimulationController extends AbstractSimulationController implements IsSimulationController {
 
+	protected static final String FILE_INPUT_NAME = "src/main/resources/linksCosts.txt";
 	// Private class for Simulation Route information
-	private static Logger logger = Logger
-			.getLogger(PSOSimulationController.class);
+	private static Logger logger = Logger.getLogger(PSOSimulationController.class);
 
 	@Override
 	public void run(final Simulation simulation) {
@@ -58,50 +63,59 @@ public class PSOSimulationController extends AbstractSimulationController
 					// Defines the node pair, the bit rate and the death time of
 					// the
 					// connection
-					Connection connection = simulation.getTrafficGenerator()
-							.createConnection(simulation);
-					simulation.getSimulationResults()
-							.incrementNumberOfRequests();
-					simulation.getSimulationResults().incrementNumberOfRequest(
-							connection.getRequestedBitRate());
+					Connection connection = simulation.getTrafficGenerator().createConnection(simulation);
+					simulation.getSimulationResults().incrementNumberOfRequests();
+					simulation.getSimulationResults().incrementNumberOfRequest(connection.getRequestedBitRate());
 					// Calculate the routes using the routing algorithm
-					List<Route> routes = simulation.getIsRoutingAlgorithm()
-							.createRoutes(connection, simulation.getTopology(),
-									simulation.getCostFunction());
+					List<Route> routes = simulation.getIsRoutingAlgorithm().createRoutes(connection,
+							simulation.getTopology(), simulation.getCostFunction());
 
 					// If routing returned at least one route solution:
 					if (routes != null && !routes.isEmpty()) {
-						RSAWrapper simulationRouteWrapper = simulation
-								.getRSAAlgorithm().getRSAWrapper(routes,
-										simulation, connection);
+						RSAWrapper simulationRouteWrapper = simulation.getRSAAlgorithm().getRSAWrapper(routes,
+								simulation, connection);
 
-						if (simulationRouteWrapper != null
-								&& simulationRouteWrapper.isValid()) {
-							connection.setRoute(simulationRouteWrapper
-									.getRoute());
-							simulation.getTopology().connect(
-									simulationRouteWrapper.getRoute());
+						if (simulationRouteWrapper != null && simulationRouteWrapper.isValid()) {
+							connection.setRoute(simulationRouteWrapper.getRoute());
+							simulation.getTopology().connect(simulationRouteWrapper.getRoute());
 							simulation.addConnection(connection);
 						} else {
 							simulation.getSimulationResults()
-									.incrementNumberOfBitRateBlockedRequest(
-											connection.getRequestedBitRate());
+									.incrementNumberOfBitRateBlockedRequest(connection.getRequestedBitRate());
 							if (simulationRouteWrapper == null) {
-								simulation
-										.getSimulationResults()
-										.incrementNumberOfNetworkBlockedRequests();
+								simulation.getSimulationResults().incrementNumberOfNetworkBlockedRequests();
 							} else if (!simulationRouteWrapper.isOSNRValid()) {
-								simulation.getSimulationResults()
-										.incrementNumberOfPhysicalBlocking();
+								simulation.getSimulationResults().incrementNumberOfPhysicalBlocking();
 							}
 						}
 					}
-					simulation.setSimulationTime(simulation
-							.getTrafficGenerator().getArrivalTimeGen()
-							.getArrivalTime(simulation));
+					simulation.setSimulationTime(
+							simulation.getTrafficGenerator().getArrivalTimeGen().getArrivalTime(simulation));
 				}
-				return simulation.getSimulationResults()
-						.getBlockingProbability();
+				return simulation.getSimulationResults().getBlockingProbability();
+			}
+
+			@Override
+			public List<Double[]> getLinkCosts() {
+				List<Double[]> linksCosts = new ArrayList<Double[]>();
+
+				try {
+					BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_INPUT_NAME));
+					for (int i = 0; i < PSOConstants.SWARM_SIZE; i++) {
+						Double[] linksCost = new Double[getProblemDimension()];
+						for (int j = 0; j < getProblemDimension(); j++) {
+							linksCost[j] = ConvertUtils.convertToDouble(bufferedReader.readLine());
+						}
+						linksCosts.add(linksCost);
+					}
+					bufferedReader.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				return linksCosts;
 			}
 		};
 		new PSOProcess().execute(problemSet);
